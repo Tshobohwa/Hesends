fafdc08e35dcb8e5a540eadf66d5201109a531e8
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-25 08:48:42 +0200

    Create transaction button

---
+++ mobile/components/buttons/TransactionButton.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+
+const TransactionButton = ({}) => {
+  return (
+    <TouchableOpacity>
+      <Text>TransactionButton</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TransactionButton;
+
+const styles = StyleSheet.create({});
