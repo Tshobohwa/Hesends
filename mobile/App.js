6a1e786afe391468b01eb8c7837419f5b76caa33
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-25 10:02:39 +0200

    Add Safe area view

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./pages/Home";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  return <Home />;
+  return (
+    <SafeAreaProvider>
+      <Home />
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
--- mobile/pages/Home.jsx
+++ mobile/pages/Home.jsx
@@ -4,6 +4,7 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
+  SafeAreaView,
 } from "react-native";
 import React from "react";
 import COLORS from "../utils/colors";
@@ -12,53 +13,57 @@ import TransactionButton from "../components/buttons/TransactionButton";
 
 const Home = () => {
   return (
-    <ScrollView style={styles.container}>
-      <TouchableOpacity
-        style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
-      >
+    <SafeAreaView
+      style={{ flex: 1, paddingVertical: 20, backgroundColor: COLORS.white }}
+    >
+      <ScrollView style={styles.container}>
+        <TouchableOpacity
+          style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+        >
+          <Text
+            style={{
+              fontWeight: "semibold",
+              color: COLORS["green-light"],
+              fontSize: 24,
+            }}
+          >
+            KES Account
+          </Text>
+          <MaterialIcons
+            name="keyboard-arrow-down"
+            size={24}
+            color={COLORS["green-light"]}
+          />
+        </TouchableOpacity>
+
         <Text
+          style={{ color: COLORS.black, fontSize: 48, fontWeight: "semibold" }}
+        >
+          14 500<Text style={{ fontSize: 24 }}> KES</Text>
+        </Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <TransactionButton name="Top up" />
+          <TransactionButton name="Send" />
+          <TransactionButton name="Withdraw" />
+        </View>
+        <View
           style={{
-            fontWeight: "semibold",
-            color: COLORS["green-light"],
-            fontSize: 24,
+            marginTop: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
           }}
         >
-          KES Account
-        </Text>
-        <MaterialIcons
-          name="keyboard-arrow-down"
-          size={24}
-          color={COLORS["green-light"]}
-        />
-      </TouchableOpacity>
-
-      <Text
-        style={{ color: COLORS.black, fontSize: 48, fontWeight: "semibold" }}
-      >
-        14 500<Text style={{ fontSize: 24 }}> KES</Text>
-      </Text>
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <TransactionButton name="Top up" />
-        <TransactionButton name="Send" />
-        <TransactionButton name="Withdraw" />
-      </View>
-      <View
-        style={{
-          marginTop: 20,
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <Text style={{ fontSize: 20, fontWeight: "semibold" }}>
-          Transactions
-        </Text>
-        <TouchableOpacity>
-          <Text style={{ fontSize: 18, color: COLORS["green-light"] }}>
-            see all
+          <Text style={{ fontSize: 20, fontWeight: "semibold" }}>
+            Transactions
           </Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 18, color: COLORS["green-light"] }}>
+              see all
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
