151a5877e2640b7d5af4d3d2af9e09a45011aa9f
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-25 09:19:26 +0200

    Change colors

--- mobile/components/buttons/TransactionButton.jsx
+++ mobile/components/buttons/TransactionButton.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import screenDimensions from "../../utils/screenDimensions";
+import COLORS from "../../utils/colors";
 
 const TransactionButton = ({ name, onPress }) => {
   return (
@@ -15,14 +16,16 @@ export default TransactionButton;
 const styles = StyleSheet.create({
   container: {
     height: 60,
-    borderWidth: 1,
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 20,
     width: (screenDimensions.screenWidth - 30) / 3,
+    borderColor: COLORS["black-light"],
+    borderWidth: 1,
   },
   text: {
     fontSize: 18,
     fontWeight: "semibold",
+    color: COLORS.black,
   },
 });
