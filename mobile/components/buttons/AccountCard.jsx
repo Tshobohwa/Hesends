776367d78102b6856bd853fdef66aeb4ce1b6f69
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-25 12:11:09 +0200

    Update card item

--- mobile/components/buttons/AccountCard.jsx
+++ mobile/components/buttons/AccountCard.jsx
@@ -5,7 +5,12 @@ import COLORS from "../../utils/colors";
 const AccountCard = () => {
   return (
     <TouchableOpacity style={styles.container}>
-      <Text>AccountCard</Text>
+      <View>
+        <Text style={[styles.text, { color: COLORS["green"] }]}>
+          Account card
+        </Text>
+        <Text style={styles.text}>Account card</Text>
+      </View>
     </TouchableOpacity>
   );
 };
@@ -14,9 +19,17 @@ export default AccountCard;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 5,
+    padding: 10,
     borderRadius: 15,
     borderWidth: 1,
     borderColor: COLORS["green-light"],
+    width: 160,
+    height: 120,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "600",
+    textAlign: "center",
   },
 });
