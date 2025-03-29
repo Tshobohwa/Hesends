80ce9b82c78db7475ac7bb4699d8b4bddbac3d07
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-29 11:31:52 +0200

    Finish select account button

--- mobile/components/buttons/SelectAccountButton.jsx
+++ mobile/components/buttons/SelectAccountButton.jsx
@@ -1,16 +1,19 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import COLORS from "../../utils/colors";
 
 const SelectAccountButton = ({ currency, balance, icon }) => {
   return (
-    <View
+    <TouchableOpacity
       style={{
-        height: 60,
         borderWidth: 1,
         borderRadius: 15,
         justifyContent: "space-between",
         padding: 10,
+        margin: 10,
+        marginVertical: 5,
         flexDirection: "row",
+        borderColor: COLORS["green-light"],
       }}
     >
       <View>
@@ -18,7 +21,7 @@ const SelectAccountButton = ({ currency, balance, icon }) => {
         <Text style={{ fontWeight: "500" }}>{balance}</Text>
       </View>
       <Image source={icon} style={{ height: 30, width: 30 }} />
-    </View>
+    </TouchableOpacity>
   );
 };
 
