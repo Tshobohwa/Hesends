95d897b4fa44eea0e70b3e746cfe9eabdbfd7b36
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-31 07:19:31 +0200

    Add logo

--- mobile/navigations/MainStackNavigator.jsx
+++ mobile/navigations/MainStackNavigator.jsx
@@ -14,7 +14,7 @@ const MainStackNavigator = () => {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false }}
-      initialRouteName="BottomTabs"
+      initialRouteName="Login"
     >
       <Stack.Screen name="BottomTabs" component={BottomTabsNavigator} />
       <Stack.Screen
--- mobile/pages/Login.jsx
+++ mobile/pages/Login.jsx
@@ -1,10 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import HederaIcon from "../images/hedera-green.png";
+import COLORS from "../utils/colors";
 
 const Login = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "flex-end",
+          gap: 4,
+          marginHorizontal: "auto",
+        }}
+      >
+        <Image
+          style={{ height: 50, width: 30, objectFit: "contain" }}
+          source={HederaIcon}
+        />
+        <Text
+          style={{
+            fontSize: 32,
+            marginBottom: -12,
+            color: COLORS["green-light"],
+          }}
+        >
+          esends
+        </Text>
+      </View>
       <Text>Login</Text>
     </SafeAreaView>
   );
@@ -12,4 +36,10 @@ const Login = () => {
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 30,
+    paddingHorizontal: 10,
+  },
+});
