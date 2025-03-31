e661ee1777fa9db681e4e47767b1e87c8cf9a946
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-31 05:56:19 +0200

    Update login and signup page for safe area view

--- mobile/pages/SignUp.jsx
+++ mobile/pages/SignUp.jsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignUp = () => {
   return (
-    <View>
+    <SafeAreaView style={{ flex: 1 }}>
       <Text>SignUp</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
